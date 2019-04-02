1c84ebe4d50523db64ca918214cc316b4d4ba49c
Alejandro Hernandez <alemarioh@gmail.com>
2019-04-01 23:22:03 -0600

    first commit

---
+++ src/components/Cart/CartTotals.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import {Link} from 'react-router-dom'
+
+const CartTotals = ({value}) => {
+  const {cartSubtotal, cartTax, cartTotal, clearCart} = value;
+
+  return (
+    <React.Fragment>
+      <div className="container">
+		<div className="row">
+		  <div className="col-10 mt-2 ml-sm-5 ml-md-auto col-sm-8 text-capitalize text-right">
+			<Link to='/'>
+			  <button className='btn btn-outline-danger text-capitalize mb-3 px-5 ' type='button' onClick={()=> clearCart()}>
+			  	clear cart
+			  </button>
+			</Link>
+			<h5>
+			  <span className="text-title">subtotal: </span>
+			  ${cartSubtotal}	
+			</h5>
+			<h5>
+			  <span className="text-title">tax: </span>
+			  ${cartTax}	
+			</h5>
+			<h5>
+			  <span className="text-title">total: </span>
+			  ${cartTotal}	
+			</h5>
+
+		  </div>
+		</div>
+      </div>
+
+    </React.Fragment>
+  )
+}
+
+export default CartTotals;
